b5e6aab00b844908f348d848c7fd21dacb6acfb2
Leonel <you@example.com>
2025-03-17 19:32:06 -0400

    oklm

--- affciherNomComplet (1).cs
+++ affciherNomComplet (1).cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git! Je m'appelle Bationo Leonel");
+        Console.WriteLine("En majuscule" + "Hello, Git! Je m'appelle Bationo Leonel".ToUpper());
     }
 }
